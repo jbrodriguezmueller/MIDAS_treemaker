e905a080da6d763bd57dbea8d8faf6e45ccc3958
b <jbrodriguezmueller@users.noreply.github.com>
2016-06-27 12:19:40 -0700

    aim 4 pep8

--- midas_treemaker.py
+++ midas_treemaker.py
@@ -4,7 +4,7 @@ import os, argparse
 import subprocess
 from collections import defaultdict
 
-def read_many_alns( list_of_files, debug=False ):
+def read_many_alns(list_of_files, debug=False):
   """Reads a list of (fasta aln) filenames, returns dict with headers:seqs"""
   all_seqs = {}
   for filex in list_of_files:
@@ -14,18 +14,17 @@ def read_many_alns( list_of_files, debug=False ):
       print "@ read_many_alns, fasta_list is "
       print fasta_list
     for fasta in fasta_list:
-      name, sequence = fasta.id, fasta.seq
-      all_seqs[ name ] = sequence
+      all_seqs[ fasta.id ] = fasta.seq
   if debug:
     print("Example sequence: ")
     print(sequence)
   return all_seqs
 
-def nominal_lens( all_seqs ):
-  all_lens = defaultdict( list )
+def nominal_lens(all_seqs):
+  all_lens = defaultdict(list)
   for key in all_seqs:
     pieces = key.split("_")
-    all_lens[ pieces[-1] ].append( len( all_seqs[key] ) )
+    all_lens[ pieces[-1] ].append(len(all_seqs[key]))
   for key in all_lens:
     print("Working on "+key)
     print("It has "+str(len(all_lens[key]))+" elements, ")
@@ -40,22 +39,23 @@ def nominal_lens( all_seqs ):
       all_lens[key] = all_lens[key][0]
   return all_lens
 
-def read_ids( input_file ):
+def read_ids(input_file):
   """Reads a file with ids, one per line, # at position 0 => comment"""
-  in_file = open( input_file )
+  in_file = open(input_file)
   in_data = []
   for rawline in in_file:
+    rawline = rawline.strip()
     if rawline[0] != "#":
-      in_data.append( rawline.strip() )
+      in_data.append(rawline)
   in_file.close()
   return in_data
 
-def query_species( all_seqs, species ):
+def query_species(all_seqs, species):
   sp_matches = {}
   for key in all_seqs:
     pieces = key.split("_")
     if species in pieces[0]:
-      sp_matches[ key ] = all_seqs[key]
+      sp_matches[key] = all_seqs[key]
   return sp_matches
 def query_protein( all_seqs, protein ):
   sp_matches = {}
@@ -65,7 +65,7 @@ def query_protein( all_seqs, protein ):
       sp_matches[ key ] = all_seqs[key]
   return sp_matches
 
-def get_species_from_db( argsdict ):
+def get_species_from_db(argsdict):
   fasta_sequences = SeqIO.parse(open(argsdict['df']),'fasta')
   in_data = read_ids( argsdict['if'] )
   out_dict = {}
@@ -135,7 +135,7 @@ def get_species_from_db( argsdict ):
         print("Fixing missing sequence, adding dashes for species="+species+" and protein="+protein)
         big_protein += good_lens[ protein ] * '-'
       else:
-        big_protein += str( goodseqs[ goodseqs.keys()[0] ] )
+        big_protein += str(goodseqs[goodseqs.keys()[0]])
     f.write(">"+species+"\n")
     f.write(big_protein+"\n")
   f.close()
@@ -143,22 +143,22 @@ def get_species_from_db( argsdict ):
     print subseqs.keys()
     print good_lens
 
-def filter_tree( argsdict ):
+def filter_tree(argsdict):
   full_aln_f = argsdict['oname'] + "/final_unfiltered.aln"
   filtered_aln_f = argsdict['oname'] + "/" + argsdict['oname'] + "_filtered_"
   filtered_aln_f+= str(round(argsdict['min_cov_position'],2))+".aln"
 
-  seq_dict = read_many_alns( [ full_aln_f ] )
+  seq_dict = read_many_alns([ full_aln_f])
   full_aln_d = {}
   full_aa_d = {}
   for key in seq_dict:
     full_aln_d[key] = str( seq_dict[key] )
     #print key, len(str(seq_dict[key]))
-  min_nt = int( argsdict['min_cov_position']*len(seq_dict) )
-  print "at least these sequences per position to be considered ... ", min_nt
+  min_nt = int(argsdict['min_cov_position']*len(seq_dict))
+  print("at least these sequences per position to be considered ... ", min_nt)
   clean_aln_d = {}
   mask_str = []
-  for i in range( len(full_aln_d[key]) ):
+  for i in range(len(full_aln_d[key])):
     this_counter = 0
     for key in full_aln_d:
       this_sequence = full_aln_d[key]
@@ -190,27 +190,27 @@ def filter_tree( argsdict ):
     f.write(out_str+"\n")
   f.close()
   print len(mask_str), len(out_str)
-  FastTreeCMD = 'FastTree -nt %s > %s_tree' % ( filtered_aln_f, filtered_aln_f )
+  FastTreeCMD = 'FastTree -nt %s > %s_tree' % (filtered_aln_f, filtered_aln_f)
   print("About to execute : "+FastTreeCMD)
-  subprocess.call( FastTreeCMD, shell=True )
+  subprocess.call(FastTreeCMD, shell=True)
   #
-  f = open( filtered_aln_f + "_tree" )
-  xxx = Phylo.read( f, format='newick' )
+  f = open(filtered_aln_f + "_tree")
+  xxx = Phylo.read(f, format='newick')
   f.close()
   #Phylo.draw( xxx )
 
-def main_seq( argsdict ):
+def main_seq(argsdict):
   try:
-    os.mkdir( argsdict['oname'] )
+    os.mkdir(argsdict['oname'])
   except OSError:
     if argsdict['overwrite'] == '0':
-      print "Error: Output directory already exists & overwrite is off, aborting."
+      print("Error: Output directory already exists & overwrite is off, aborting.")
       exit(-1)
     else:
-      print "Notice: Output directory already exists & overwrite is on, continuing."
+      print("Notice: Output directory already exists & overwrite is on, continuing.")
 
-  get_species_from_db( argsdict )
-  filter_tree( argsdict )
+  get_species_from_db(argsdict)
+  filter_tree(argsdict)
 
 def main():
 
@@ -244,7 +244,7 @@ def main():
   args = parser.parse_args()
   argsdict = vars(args)
 
-  main_seq( argsdict )
+  main_seq(argsdict)
 
 if __name__ == "__main__":
   main()
